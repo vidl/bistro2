df08c0719ed631216e18df9a8f83afa7d3beea92
David Nyffenegger <david.nyffenegger@ergon.ch>
2014-09-29 08:34:31 +0200

    First version of the cashbox page

--- client/cashbox/cashbox.js
+++ client/cashbox/cashbox.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('bistro.cashbox', ['ui.router'])
+angular.module('bistro.cashbox', ['ui.router', 'bistro.articles'])
 
     .config(['$stateProvider', function ($stateProvider) {
         $stateProvider.state('cashbox', {
@@ -11,6 +11,31 @@ angular.module('bistro.cashbox', ['ui.router'])
 
     }])
 
-    .controller('CashboxCtrl', [function () {
+    .controller('CashboxCtrl', ['$scope', 'Article', '$http', function ($scope, Article, $http) {
+        $scope.articles = Article.query();
 
+        $scope.availability = {};
+        $http.get('/availability').success(function(data){
+            $scope.availability = data;
+        });
+
+        $scope.available = function(article){
+            var available = _.map(article.limits, function(limit){
+                var availableByLimit = $scope.availability[limit.limit] || { used: 0, total: 0};
+                return Math.floor((availableByLimit.total - availableByLimit.used) / limit.dec);
+            });
+            return available.length ? _.min(available) : undefined;
+        };
+
+        $scope.order = {};
+        $http.get('/order').success(function(data){
+            $scope.order = data;
+        });
+
+        $scope.inc = function(article){
+            $http.put('/order/inc', {article: article._id}).success(function(data){
+                $scope.order = data.order;
+                $scope.availability = data.limits;
+            });
+        };
     }]);
