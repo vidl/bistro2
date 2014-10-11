f165bd0286d6d172ec49c9c35259eb73b7a38ed2
David Nyffenegger <david.nyffenegger@ergon.ch>
2014-10-11 20:29:42 +0200

    Show number of pending print jobs in menu

--- client/printJobs/printJobs.js
+++ client/printJobs/printJobs.js
@@ -16,7 +16,7 @@ angular.module('bistro.printJobs', ['ui.router','ngResource', 'bistro.date'])
     .filter('translateTypes', function(){
         var types = {
             kitchen: 'Küche',
-            receipt: 'Bon'
+            receipt: 'Beleg'
         };
         return function(type){
           return types[type] || 'unbekannt';
@@ -27,12 +27,36 @@ angular.module('bistro.printJobs', ['ui.router','ngResource', 'bistro.date'])
         return $resource('/api/v1/printJobs/:printJobId',{printJobId: '@_id'});
     }])
 
+    .service('printJobs', ['PrintJob', '$interval', function(PrintJob, $interval){
+        var printJobs = [];
+        $interval(function(){
+            PrintJob.query({sort: '-updatedAt'}, function(data){
+                while(printJobs.length > 0) {
+                    printJobs.pop();
+                }
+                printJobs.push.apply(printJobs, data);
+            });
+        }, 1000);
+        return printJobs;
+    }])
 
-    .controller('PrintJobsCtrl', ['$scope', 'PrintJob', '$http', function ($scope, PrintJob, $http) {
-        $scope.printJobs = PrintJob.query({sort: '-updatedAt'});
+    .filter('pendingJobsCount', ['printJobs', function(printJobs){
+        return function() {
+            var pendingJobsCount = _.reduce(printJobs, function(count, printJob){
+                return count + (printJob.pending ? 1 : 0);
+            },0);
+            return pendingJobsCount || undefined;
+        }
+    }])
+
+    .controller('PrintJobsCtrl', ['$scope', 'printJobs', '$http', function ($scope, printJobs, $http) {
+        $scope.printJobs = printJobs;
         $scope.cancel = function(printJob){
             $http.post('/printJob/cancel', {printJob: printJob._id}).success(function(){
-                $scope.printJobs = PrintJob.query({sort: '-updatedAt'});
+                var index = $scope.printJobs.indexOf(printJob);
+                if (index > -1) {
+                    $scope.printJobs.splice(index, 1);
+                }
             });
         };
 
