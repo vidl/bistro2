dc743b9fb9de8ab780c351076621d7ad1c64ff2a
David Nyffenegger <david.nyffenegger@ergon.ch>
2014-11-01 18:48:26 +0100

    Binds webserver to 0.0.0.0

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ var app = bistro('mongodb://127.0.0.1:27017/bistro');
 app.use(express.static('client'));
 app.use('/pdfs', express.static(__dirname + '/pdfs'));
 
-http.createServer(app).listen(port, function() {
+http.createServer(app).listen(port, '0.0.0.0', function() {
     console.log('Express server listening on port ' + port);
 });
